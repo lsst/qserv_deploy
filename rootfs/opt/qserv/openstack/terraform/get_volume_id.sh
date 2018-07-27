4993439cff520969cde27096f01454f5f7c56b0b
ClementLaneres <32100747+ClementLaneres@users.noreply.github.com>
2018-07-27 15:38:03 +0200

    Update get_volume_id.sh

--- rootfs/opt/qserv/openstack/terraform/get_volume_id.sh
+++ rootfs/opt/qserv/openstack/terraform/get_volume_id.sh
@@ -2,27 +2,8 @@
 # Number of first volume
 export TF_VAR_firstVolume=100
 # Get volume list
-openstack volume list | grep vol-qserv >> volume.txt
 
-nb_ligne=`wc -l $CLUSTER_CONFIG_DIR"/terraform/volume.txt"| grep -o "^[0-9]\+"`
-volume_id="{\""
-nb_ligne=$(($nb_ligne -1))
-for i in `seq 1 ${nb_ligne}`
-do
-	ligne=`sed -n "${i} p" "$CLUSTER_CONFIG_DIR/terraform/volume.txt"`
-	ID=`(expr substr "$ligne" 3 36)`
-	nb_volume=`(expr substr "$ligne" 41 30)`
-	nb_volume=`echo ${nb_volume} | cut -d'|' -f1 | grep -o "[0-9]\+"`
-	volume_id=${volume_id}${nb_volume}"\" = \"${ID}\",\""
-done
-
-nb_ligne=$(($nb_ligne + 1))
-ligne=`sed -n "${nb_ligne} p" "$CLUSTER_CONFIG_DIR/terraform/volume.txt"`
-ID=`(expr substr "$ligne" 3 36)`
-nb_volume=`(expr substr "$ligne" 41 30)`
-nb_volume=`echo ${nb_volume} | cut -d'|' -f1 | grep -o "[0-9]\+"`
-volume_id=${volume_id}${nb_volume}"\" = \"${ID}\"}"
-
+volume_list=`openstack volume list -c ID -c Display\ Name -f csv | grep vol-qserv | tr , =`
+volume_list=`echo ${volume_list} | sed -e 's/\"\ \"/\",\"/g'`
+volume_list={${volume_list}}
 export TF_VAR_volumeId="${volume_id}"
-
-rm volume.txt
