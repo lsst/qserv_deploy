22719f84dde5afa54e4885477c4991beb97833d2
ClementLaneres <32100747+ClementLaneres@users.noreply.github.com>
2018-07-27 12:32:02 +0200

    Update get_volume_id.sh

--- rootfs/opt/qserv/openstack/terraform/get_volume_id.sh
+++ rootfs/opt/qserv/openstack/terraform/get_volume_id.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
-
 # Number of first volume
-echo "first_volume = 100" >> $CLUSTER_CONFIG_DIR/terraform.tfvars
+export TF_VAR_firstVolume=100
 # Get volume list
 openstack volume list | grep vol-qserv >> volume.txt
 
-# Add volume in variables.tf
-echo "# Volume id
-volume_id = {">> $CLUSTER_CONFIG_DIR/terraform.tfvars
-
 nb_ligne=`wc -l $CLUSTER_CONFIG_DIR"/terraform/volume.txt"| grep -o "^[0-9]\+"`
-
+volume_id="{\""
+nb_ligne=$(($nb_ligne -1))
 for i in `seq 1 ${nb_ligne}`
 do
 	ligne=`sed -n "${i} p" "$CLUSTER_CONFIG_DIR/terraform/volume.txt"`
 	ID=`(expr substr "$ligne" 3 36)`
 	nb_volume=`(expr substr "$ligne" 41 30)`
 	nb_volume=`echo ${nb_volume} | cut -d'|' -f1 | grep -o "[0-9]\+"`
-	echo "\"${nb_volume}\"=\"${ID}\"" >> $CLUSTER_CONFIG_DIR/terraform.tfvars
+	volume_id=${volume_id}${nb_volume}"\" = \"${ID}\",\""
 done
-echo "}" >> $CLUSTER_CONFIG_DIR/terraform.tfvars
+
+nb_ligne=$(($nb_ligne + 1))
+ligne=`sed -n "${nb_ligne} p" "$CLUSTER_CONFIG_DIR/terraform/volume.txt"`
+ID=`(expr substr "$ligne" 3 36)`
+nb_volume=`(expr substr "$ligne" 41 30)`
+nb_volume=`echo ${nb_volume} | cut -d'|' -f1 | grep -o "[0-9]\+"`
+volume_id=${volume_id}${nb_volume}"\" = \"${ID}\"}"
+
+export TF_VAR_volumeId="${volume_id}"
 
 rm volume.txt
