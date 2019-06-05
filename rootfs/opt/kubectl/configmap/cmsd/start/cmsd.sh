aabf1a867e9fbc8b57f8ab3afd6f9bc038c64637
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-06-05 09:28:23 +0200

    Force cmsd startup script to wait for mysql startup

--- rootfs/opt/kubectl/configmap/cmsd/start/cmsd.sh
+++ rootfs/opt/kubectl/configmap/cmsd/start/cmsd.sh
@@ -30,6 +30,18 @@ else
     MYSQLD_SOCKET="/qserv/data/mysql/mysql.sock"
     XRDSSI_CONFIG="$CONFIG_DIR/xrdssi.cf"
 
+    # Wait for local mysql to be configured and started
+    while true; do
+        if mysql --socket "$MYSQLD_SOCKET" --user="$MYSQLD_USER_QSERV"  --skip-column-names \
+            -e "SELECT CONCAT('Mariadb is up: ', version())"
+        then
+            break
+        else
+            echo "Wait for MySQL startup"
+        fi
+        sleep 2
+    done
+
     # Wait for xrootd master reachability
     until timeout 1 bash -c "cat < /dev/null > /dev/tcp/${XROOTD_DN}/1094"
     do
